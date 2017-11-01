4d60b729edf8259a897baa63810d1456cbd07a06
Marco Debac <marco.debac@gmail.com>
2017-11-01 17:51:40 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import { logger } from './lib/logging';
+import { MockServer } from './lib/mock-server';
+import { AdministrationServer } from './lib/administration-server';
+
+const mockServer = new MockServer(3000, "Wow, even more amazing!!");
+mockServer.start();
+
+const administrationServer = new AdministrationServer(3001);
+administrationServer.start();
+
+setInterval(() => {
+    logger.debug("Test");
+}, 5000);
